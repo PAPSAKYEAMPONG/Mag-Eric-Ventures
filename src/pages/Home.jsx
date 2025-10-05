aa78f57fd24e91080c631429c35ae6a273ce146f
PAPSAKYEAMPONG <papaankama1@gmail.com>
2025-10-05 18:54:27 +0000

    Hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,11 +27,11 @@ const Home = () => {
     <section className="hero">
       <div className="hero-content">
         <h1>
-          Welcome to <span>Mag-Eric Ventures</span>
+          Welcome to <br></br> <span>Mag-Eric Ventures</span>
         </h1>
         <p>
-          Your trusted UK-to-Ghana store for premium biscuits, breakfast cereals,
-          body essentials, and household products — all at affordable prices.
+          Your trusted UK-to-Ghana store <br></br>Premium biscuits, breakfast cereals,
+          body essentials, and household products all at affordable prices.
         </p>
         <button className="shop-btn">Shop Now</button>
       </div>
