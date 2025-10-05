f34ca982ffbb02acfdf3e356aef39ffb38f270ff
PAPSAKYEAMPONG <papaankama1@gmail.com>
2025-10-05 03:20:08 +0000

    hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import "../main.css"; // Replace with your actual image path
 
 const Home = () => {
   return (
-    <div style={{ paddingTop: '120px' }}>
-      <h1>Home Page</h1>
-    </div>
+    <section className="hero">
+      <div className="hero-content">
+        <h1>
+          Welcome to <span><br></br>Mag-Eric Ventures</span>
+        </h1>
+        <p>
+          Your trusted UK-to-Ghana store <br></br>Premium biscuits, breakfast cereals,
+          body essentials, and household products <br></br>All at affordable prices.
+        </p>
+        <button className="shop-btn">Shop Now</button>
+      </div>
+
+      <div className="hero-image"></div>
+    </section>
   );
 };
 
