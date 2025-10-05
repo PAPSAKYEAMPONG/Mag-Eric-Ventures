d27654f010a1f10b1270d0dc19bc132ea2ccb928
PAPSAKYEAMPONG <papaankama1@gmail.com>
2025-10-05 17:42:54 +0000

    hero transition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,28 @@
-import "../main.css"; // Replace with your actual image path
+import { useState, useEffect } from 'react';
+import "../main.css";
+import flaxseed from '../assets/flaxseed.jpg';
+import kreteaz from '../assets/kreteaz.jpg';
+import Avocadooil from '../assets/avocadooil.jpg';
+import olaycoconutoil from '../assets/olaycoconutoil.jpg';
+import pepper from '../assets/pepper.jpg';
+import garlicherb from '../assets/garlicherb.jpg';
+import irishspring from '../assets/irishspring.jpg';
+import softner from '../assets/softner.jpg';
+import Nescafe from '../assets/Nescafe.jpg';
+
+const images = [kreteaz, flaxseed, Avocadooil, olaycoconutoil, pepper, garlicherb, irishspring, softner, Nescafe];
 
 const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <section className="hero">
       <div className="hero-content">
@@ -14,7 +36,10 @@ const Home = () => {
         <button className="shop-btn">Shop Now</button>
       </div>
 
-      <div className="hero-image"></div>
+      <div
+        className="hero-image"
+        style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
+      ></div>
     </section>
   );
 };
